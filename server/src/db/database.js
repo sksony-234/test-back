77120e9c3daa0716d63097ff3659c7d6b4d77af4
sksony <sksony2698@gmail.com>
2023-01-10 18:58:37 +0530

    chage uri

--- server/src/db/database.js
+++ server/src/db/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect("mongodb+srv://Sksony:SksonyMongo5@cluster0.aq4woat.mongodb.net/todos?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log("Database connected sucessfully")).catch((err) => console.log(err));
